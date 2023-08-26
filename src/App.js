f7d74e43f15d1d6740e1a55a691cf07a632a21f7
rahulanand <rahulanand4110@gmail.com>
2023-08-26 18:36:08 +0530

    small project on cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css'
+import ProfileCard from "./ProfileCard"
+import AlexaImage from "./images/alexa.png"
+import CortanaImage from "./images/cortana.png"
+import SiriImage from "./images/siri.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistance</p>
+                </div>
+            </section>
+
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                                title="Alexa" 
+                                handle="@alexa99" 
+                                image={AlexaImage}
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                                title="Cortana" 
+                                handle="@cortana32" 
+                                image={CortanaImage}
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                                title="Siri" 
+                                handle="@siri01" 
+                                image={SiriImage}
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
 }
-
-export default App;
